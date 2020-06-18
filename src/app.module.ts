aaf641bbce40bc6ee469e892a998439dc3637f52
hota1024 <hotalog@hota1024.com>
2020-06-18 18:09:52 +0900

    🏷️ Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
+import * as path from 'path'
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 8889,
+      username: 'root',
+      password: 'root',
+      database: 'nest-learn-1',
+      entities: [path.join(__dirname, '**/**.entity{.ts,.js}')],
+      synchronize: true,
+      keepConnectionAlive: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
